da7eeb55ed54e2537645ba354a7f78076f510eb7
Nathan <nateinthegame@gmail.com>
2018-09-17 14:05:56 -0700

    AXIOS POST to Documents

--- src/actions/TextEditor.js
+++ src/actions/TextEditor.js
@@ -1,6 +1,29 @@
 import C from '../constants'
+import axios from 'axios'
+const qs = require('qs')
+const Axios = axios.create({
+    baseURL: 'https://voices-of-terminus-db.herokuapp.com/',
+    timeout: 1000,
+    headers: {
+      'Content-type': 'application/x-www-form-urlencoded'
+  }
+})
+
+
 
 export const setEditorState = (editorState) => ({
     type: C.SET_EDITOR_STATE,
     payload: editorState
  })
+
+ export function postEditorState(title){
+    Axios.post('admin/api/v1/articles/',
+     qs.stringify({ 'title': title })
+   )
+   .then(response => {
+     console.log(response);
+   })
+   .catch(error => {
+     console.log(error);
+   })
+ }
--- src/views/Articles/index.js
+++ src/views/Articles/index.js
@@ -6,6 +6,7 @@ import './styles.css'
 import './stylesM.css'
 import Card from '../../components/Card'
 import TextEditor from '../../components/TextEditor'
+import {postEditorState} from '../../actions/TextEditor'
 
 const mapStateToProps = ({}) => ({
 })
@@ -51,6 +52,7 @@ class Articles extends Component {
   /* render() */
 
   componentDidMount() {
+    postEditorState('AXIOS FTW')
   }
   
   componentWillReceiveProps(nextProps) {
