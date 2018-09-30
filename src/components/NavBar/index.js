9acdb1a06318304a560e37235b808cc8dbb5d9b8
Nathan <nateinthegame@gmail.com>
2018-09-29 18:36:27 -0700

    Styles

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -71,7 +71,7 @@ class NavBar extends Component {
         <Navbar inverse collapseOnSelect className="NavBar">
           <Navbar.Header>
             <Navbar.Brand>
-              <LinkContainer to ="/">
+              <LinkContainer to="/">
                 <NavItem eventKey={1}><Image src={votLogo} height="50px" onMouseOver={e => e.currentTarget.src = votLogoHover} onMouseLeave={e => e.currentTarget.src = votLogo}/></NavItem>
               </LinkContainer>
             </Navbar.Brand>
@@ -79,23 +79,23 @@ class NavBar extends Component {
           </Navbar.Header>
           <Navbar.Collapse>
             <Nav>
-              <LinkContainer to ="/articles"><NavItem eventKey={2}>ARTICLES</NavItem></LinkContainer>
-              <LinkContainer to ="/calendar"><NavItem eventKey={3}>CALENDAR</NavItem></LinkContainer>
-              <LinkContainer to ="/news/1"><NavItem eventKey={4}>NEWS</NavItem></LinkContainer>
+              <LinkContainer to="/articles"><NavItem eventKey={2}>ARTICLES</NavItem></LinkContainer>
+              <LinkContainer to="/calendar"><NavItem eventKey={3}>CALENDAR</NavItem></LinkContainer>
+              <LinkContainer to="/news/1"><NavItem eventKey={4}>NEWS</NavItem></LinkContainer>
               <NavDropdown eventKey={5} title="GUILD" id="basic-nav-dropdown">
-                <LinkContainer to ="/guild/about"><NavItem eventKey={5.1}>ABOUT</NavItem></LinkContainer>
-                <LinkContainer to ="/guild/roster"><NavItem eventKey={5.2}>ROSTER</NavItem></LinkContainer>
-                <LinkContainer to ="/guild/charters"><NavItem eventKey={5.3}>CHARTERS</NavItem></LinkContainer>
-                <LinkContainer to ="/guild/lore"><NavItem eventKey={5.4}>LORE</NavItem></LinkContainer>
-                <LinkContainer to ="/guild/contests"><NavItem eventKey={5.5}>CONTESTS</NavItem></LinkContainer>
-                <LinkContainer to ="/guild/team"><NavItem eventKey={5.6}>TEAM</NavItem></LinkContainer>
-                <LinkContainer to ="/guild/join"><NavItem eventKey={5.7}>JOIN</NavItem></LinkContainer>
+                <LinkContainer to="/guild/about"><NavItem eventKey={5.1}>ABOUT</NavItem></LinkContainer>
+                <LinkContainer to="/guild/roster"><NavItem eventKey={5.2}>ROSTER</NavItem></LinkContainer>
+                <LinkContainer to="/guild/charters"><NavItem eventKey={5.3}>CHARTERS</NavItem></LinkContainer>
+                <LinkContainer to="/guild/lore"><NavItem eventKey={5.4}>LORE</NavItem></LinkContainer>
+                <LinkContainer to="/guild/contests"><NavItem eventKey={5.5}>CONTESTS</NavItem></LinkContainer>
+                <LinkContainer to="/guild/team"><NavItem eventKey={5.6}>TEAM</NavItem></LinkContainer>
+                <LinkContainer to="/guild/join"><NavItem eventKey={5.7}>JOIN</NavItem></LinkContainer>
               </NavDropdown>
               <NavDropdown eventKey={6} title="MEDIA" id="basic-nav-dropdown">
-              <LinkContainer to ="/media/images"><NavItem eventKey={6.1}>IMAGES</NavItem></LinkContainer>
-              <LinkContainer to ="/media/videos"><NavItem eventKey={6.2}>VIDEOS</NavItem></LinkContainer>
-              <LinkContainer to ="/media/streams"><NavItem eventKey={6.3}>STREAMS</NavItem></LinkContainer>
-              <LinkContainer to ="/media/podcasts"><NavItem eventKey={6.4}>PODCASTS</NavItem></LinkContainer>
+              <LinkContainer to="/media/images"><NavItem eventKey={6.1}>IMAGES</NavItem></LinkContainer>
+              <LinkContainer to="/media/videos"><NavItem eventKey={6.2}>VIDEOS</NavItem></LinkContainer>
+              <LinkContainer to="/media/streams"><NavItem eventKey={6.3}>STREAMS</NavItem></LinkContainer>
+              <LinkContainer to="/media/podcasts"><NavItem eventKey={6.4}>PODCASTS</NavItem></LinkContainer>
             </NavDropdown>        
               <NavDropdown eventKey={7} title="VR" id="connect-nav-dropdown">
                 <Image src={vrLogo} className="vrLogo"/>
