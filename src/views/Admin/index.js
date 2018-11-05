eb6270f1d161c2fca9f7487c15dc7532b9f3d79b
Nathan <nateinthegame@gmail.com>
2018-11-05 10:47:26 -0800

    Table Footer

--- src/views/Admin/index.js
+++ src/views/Admin/index.js
@@ -15,13 +15,28 @@ import {statusLevelInt, statusLevelString} from '../../helpers/helpers'
 const columns = [
   {Header: 'INFO', columns: [
     {Header: 'Username', accessor: 'username', filterMethod: (filter, rows) => matchSorter(rows, filter.value, { keys: [filter.id] }), filterAll: true, maxWidth: 125,
-      Cell: props => (<Link to={'admin/user/profile/' + props.original.id}>{props.value}</Link>)},
+    Footer: Users => (
+      <span style={{color: 'var(--primaryColor)'}}>
+        <strong>Total: {Users.data.length}</strong>
+      </span>
+    ),
+    Cell: props => (<Link to={'admin/user/profile/' + props.original.id}>{props.value}</Link>)},
     {Header: 'Email', accessor: 'email', filterMethod: (filter, rows) => matchSorter(rows, filter.value, { keys: [filter.id] }), filterAll: true},
   ]},
   {Header: 'Permissions', columns: [
     {Header: 'Admin?', accessor: 'is_superuser', filterMethod: (filter, rows) => matchSorter(rows, filter.value, { keys: [filter.id] }), filterAll: true, maxWidth: 75,
+    Footer: Users => (
+      <span style={{color: 'var(--primaryColor)'}}>
+        <strong>Total: {Users.data.filter(user => user.is_superuser).length}</strong>
+      </span>
+    ),
     Cell: props => String(props.value)},
   {Header: 'Mod?', accessor: 'is_staff', filterMethod: (filter, rows) => matchSorter(rows, filter.value, { keys: [filter.id] }), filterAll: true, maxWidth: 75,
+  Footer: Users => (
+    <span style={{color: 'var(--primaryColor)'}}>
+      <strong>Total: {Users.data.filter(user => user.is_staff).length}</strong>
+    </span>
+  ),
     Cell: props => String(props.value)},
   {Header: 'Status',  id: 'status', accessor: User => statusLevelInt({is_leader: User.is_leader, is_council: User.is_council, is_general_officer: User.is_general_officer, 
     is_officer: User.is_officer, is_senior_member: User.is_senior_member, is_junior_member: User.is_junior_member, is_recruit: User.is_recruit}), filterMethod: (filter, rows) => matchSorter(rows, filter.value[1], { keys: [filter.id] }), filterAll: true, maxWidth: 125,
@@ -38,7 +53,12 @@ const columns = [
     Cell: props => <Moment format="YYYY-MM-DD">{props.value}</Moment>, filterMethod: (filter, rows) => matchSorter(rows, filter.value, { keys: [filter.id] }), filterAll: true},
   {Header: 'Joined', accessor: 'date_joined', maxWidth: 100,
     Cell: props => <Moment format="YYYY-MM-DD">{props.value}</Moment>,filterMethod: (filter, rows) => matchSorter(rows, filter.value, { keys: [filter.id] }), filterAll: true},
-  {Header: 'XP', accessor: 'experience_points', filterMethod: (filter, rows) => matchSorter(rows, filter.value, { keys: [filter.id] }), filterAll: true, maxWidth: 80},
+  {Header: 'XP', accessor: 'experience_points', filterMethod: (filter, rows) => matchSorter(rows, filter.value, { keys: [filter.id] }), filterAll: true, maxWidth: 100,
+  Footer: Users => (
+    <span style={{color: 'var(--primaryColor)'}}>
+      <strong>Max: {Math.max(...Users.data.map(user => user.experience_points))}</strong>
+    </span>
+  )},
   ]}
 ]
 
@@ -132,6 +152,15 @@ class Admin extends Component {
             showPageSizeOptions
             showPaginationBottom
             showPageJump
+            defaultSorted={[
+              {
+                id: "status",
+                desc: true
+              }
+            ]}
+            defaultPageSize={15}
+            pageSizeOptions={[5, 10, 15, 20, 50, 100]}
+            multiSort={true}
             // getTrProps = {
             //   (state, rowInfo) => {
             //     console.log("state: ", state)
