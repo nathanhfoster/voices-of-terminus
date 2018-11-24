04579c99ad33634c66e0396a9eda1e50a3a09075
Nathan <nateinthegame@gmail.com>
2018-11-24 14:57:35 -0800

    Update Articles.js

--- src/actions/Articles.js
+++ src/actions/Articles.js
@@ -87,11 +87,7 @@ export const postArticleLike = (token, payload) => {
         dispatch ({
           type: C.GET_HTML_DOCUMENT,
           payload: HtmlDocument
-        })
-        dispatch({
-          type: C.SET_API_RESPONSE,
-          payload: res
-        })
+        }) 
       }).catch((e) => console.log(e))
 }
 
@@ -116,11 +112,7 @@ export const postArticleComment = (token, payload) => {
         dispatch ({
           type: C.GET_HTML_DOCUMENT,
           payload: HtmlDocument
-        })
-        dispatch({
-          type: C.SET_API_RESPONSE,
-          payload: res
-        })
+        }) 
       }).catch((e) => dispatch({
         type: C.SET_API_RESPONSE,
         payload: e.response
