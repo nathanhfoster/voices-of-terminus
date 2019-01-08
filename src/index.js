347c4a549d7d8fb6699532d4e7747cb61ae1ea74
Nathan <nateinthegame@gmail.com>
2019-01-07 23:17:50 -0800

    isQuotaExceeded

--- src/index.js
+++ src/index.js
@@ -72,28 +72,27 @@ const saveState = () => {
       // localStorage.setItem("redux-store", state);
     }
   }
-
-  const isQuotaExceeded = e => {
-    let quotaExceeded = false;
-    if (e) {
-      if (e.code) {
-        switch (e.code) {
-          case 22:
+};
+const isQuotaExceeded = e => {
+  let quotaExceeded = false;
+  if (e) {
+    if (e.code) {
+      switch (e.code) {
+        case 22:
+          quotaExceeded = true;
+          break;
+        case 1014:
+          // Firefox
+          if (e.name === "NS_ERROR_DOM_QUOTA_REACHED") {
             quotaExceeded = true;
-            break;
-          case 1014:
-            // Firefox
-            if (e.name === "NS_ERROR_DOM_QUOTA_REACHED") {
-              quotaExceeded = true;
-            }
-            break;
-        }
-      } else if (e.number === -2147024882) {
-        // Internet Explorer 8
-        quotaExceeded = true;
+          }
+          break;
       }
+    } else if (e.number === -2147024882) {
+      // Internet Explorer 8
+      quotaExceeded = true;
     }
-    return quotaExceeded;
-  };
+  }
+  return quotaExceeded;
 };
 store.subscribe(saveState);
