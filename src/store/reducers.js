633b21b1332bd8646e77f719c2e945fc51b42467
Nathan <nateinthegame@gmail.com>
2019-01-08 10:10:23 -0800

    pushMessages false

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -38,71 +38,71 @@ export const DiscordData = (state = {}, action) =>
 export const editorState = (state = null, action) =>
   action.type === C.SET_EDITOR_STATE ? action.payload : state;
 
-  export const Articles = (state = { results: [] }, action) => {
-    switch (action.type) {
-      case C.GET_ARTICLES_LOADING:
-        return {
-          ...state,
-          loading: true,
-          loaded: false
-        };
-      case C.GET_ARTICLES_SUCCESS:
-        const { posting, posted, updating, updated } = state;
-        return {
-          ...action.payload,
-          loading: false,
-          loaded: true,
-          posting,
-          posted,
-          updating,
-          updated,
-          error: null
-        };
-      case C.GET_ARTICLES_ERROR:
-        return {
-          ...state,
-          loading: false,
-          loaded: true,
-          error: action.payload
-        };
-      case C.POST_ARTICLES_LOADING:
-        return {
-          ...state,
-          posting: true,
-          posted: false
-        };
-      case C.POST_ARTICLES_SUCCESS:
-        return {
-          ...state,
-          posting: false,
-          posted: true,
-          error: null
-        };
-      case C.UPDATE_ARTICLES_LOADING:
-        return {
-          ...state,
-          updating: true,
-          updated: false
-        };
-      case C.UPDATE_ARTICLES_SUCCESS:
-        return {
-          ...state,
-          updating: false,
-          updated: true,
-          error: null
-        };
-      case C.CLEAR_ARTICLES_API:
-        return {
-          ...state,
-          posting: false,
-          posted: false,
-          updating: false,
-          updated: false
-        };
-      default:
-        return state;
-    }
-  };
+export const Articles = (state = { results: [] }, action) => {
+  switch (action.type) {
+    case C.GET_ARTICLES_LOADING:
+      return {
+        ...state,
+        loading: true,
+        loaded: false
+      };
+    case C.GET_ARTICLES_SUCCESS:
+      const { posting, posted, updating, updated } = state;
+      return {
+        ...action.payload,
+        loading: false,
+        loaded: true,
+        posting,
+        posted,
+        updating,
+        updated,
+        error: null
+      };
+    case C.GET_ARTICLES_ERROR:
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        error: action.payload
+      };
+    case C.POST_ARTICLES_LOADING:
+      return {
+        ...state,
+        posting: true,
+        posted: false
+      };
+    case C.POST_ARTICLES_SUCCESS:
+      return {
+        ...state,
+        posting: false,
+        posted: true,
+        error: null
+      };
+    case C.UPDATE_ARTICLES_LOADING:
+      return {
+        ...state,
+        updating: true,
+        updated: false
+      };
+    case C.UPDATE_ARTICLES_SUCCESS:
+      return {
+        ...state,
+        updating: false,
+        updated: true,
+        error: null
+      };
+    case C.CLEAR_ARTICLES_API:
+      return {
+        ...state,
+        posting: false,
+        posted: false,
+        updating: false,
+        updated: false
+      };
+    default:
+      return state;
+  }
+};
 
 export const Newsletters = (state = { results: [] }, action) => {
   switch (action.type) {
@@ -237,7 +237,7 @@ export const Admin = (state = {}, action) => {
 };
 
 export const Settings = (
-  state = { showFooter: true, pushMessages: true },
+  state = { showFooter: true, pushMessages: false },
   action
 ) => {
   switch (action.type) {
