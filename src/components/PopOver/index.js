859094baab38bd0fc742fd5e4b292c81ebc4d83f
Nathan Foster <nathan.foster@accenture.com>
2019-02-27 07:12:52 -0800

    Functional

--- src/components/PopOver/index.js
+++ src/components/PopOver/index.js
@@ -1,71 +1,28 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { Row, Col, OverlayTrigger, Popover } from "react-bootstrap";
-import { connect as reduxConnect } from "react-redux";
+import React from "react";
+import { OverlayTrigger, Popover } from "react-bootstrap";
 import "./styles.css";
 
-const mapStateToProps = ({}) => ({});
+const PopOver = props => {
+  const { User, children } = props;
+  const { token } = User;
 
-const mapDispatchToProps = {};
-
-class PopOver extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  static propTypes = {};
-
-  static defaultProps = {};
-
-  componentWillMount() {
-    this.getState(this.props);
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    return true;
-  }
-
-  componentWillUpdate() {}
-
-  /* render() */
-
-  componentDidMount() {}
-
-  componentWillReceiveProps(nextProps) {
-    this.getState(nextProps);
-  }
-
-  getState = props => {
-    const { User, children } = props;
-    this.setState({ User, children });
-  };
-
-  componentDidUpdate(prevProps, prevState) {}
-
-  componentWillUnmount() {}
-
-  render() {
-    const { User, children } = this.state;
-    const { token } = User;
-
-    const popover = (
-      <Popover id="popover-basic" title={null}>
-        {React.Children.map(children, child => (
-          <div className="PopRow">{child}</div>
-        ))}
-      </Popover>
-    );
-    return token ? (
-      <OverlayTrigger trigger="click" placement="bottom" overlay={popover}>
-        <div
-          variant="success"
-          className="ActionDots pull-right"
-          onClick={e => e.stopPropagation()}
-        >
-          <i className="fas fa-ellipsis-v pull-left" variant="success" />
-        </div>
-      </OverlayTrigger>
-    ) : null;
-  }
-}
-export default reduxConnect(mapStateToProps, mapDispatchToProps)(PopOver);
+  const popover = (
+    <Popover id="popover-basic" title={null}>
+      {React.Children.map(children, child => (
+        <div className="PopRow">{child}</div>
+      ))}
+    </Popover>
+  );
+  return token ? (
+    <OverlayTrigger trigger="click" placement="bottom" overlay={popover}>
+      <div
+        variant="success"
+        className="ActionDots pull-right"
+        onClick={e => e.stopPropagation()}
+      >
+        <i className="fas fa-ellipsis-v pull-left" variant="success" />
+      </div>
+    </OverlayTrigger>
+  ) : null;
+};
+export default PopOver;
