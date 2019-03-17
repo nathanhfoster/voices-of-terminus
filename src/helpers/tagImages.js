cb76d827dfb39a6c21cddd5bbfa4f0f8df903878
Nathan <nateinthegame@gmail.com>
2019-03-16 17:27:40 -0700

    Article Default Image

--- src/helpers/tagImages.js
+++ src/helpers/tagImages.js
@@ -1,17 +1,21 @@
+import ArticleDefault from "../images/Articles/default.png";
+
 import Community from "../images/News/community.png";
-import Default from "../images/News/defualt.png";
+import NewsDefault from "../images/News/default.png";
 import Game from "../images/News/game.png";
 import Guild from "../images/News/guild.png";
 import Official from "../images/News/official.png";
 import Show from "../images/News/show.png";
+import VoT from "../images/News/vot.png";
 
 export const tagImage = tags => {
+  const Default = tags.includes("Newsletter") ? NewsDefault : ArticleDefault;
   if (tags.includes("VR")) return Official;
   if (tags.includes("Guild")) return Guild;
   if (tags.includes("Game")) return Game;
   if (tags.includes("Show")) return Show;
   if (tags.includes("Community")) return Community;
-  if (tags.includes("VoT")) return Default;
+  if (tags.includes("VoT")) return VoT;
   //   if (tags.includes("Blog"))
   //   if (tags.includes("FanMade"))
   //   if (tags.includes("Guide"))
